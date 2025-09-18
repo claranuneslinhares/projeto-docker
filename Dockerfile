0a4ce4224ffd6206d561940fafef0b057ddff3e0
claranuneslinhares <mclaranunes667@gmail.com>
2025-09-18 16:59:01 -0300

    mudanças no main.py e docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.10-slim
+FROM python:3.10
 
-WORKDIR / app
+WORKDIR /app
 
-COPY ..
-
-RUN apt-get update && \ apt-get install -y default-libmysqlclient-dev gcc && \ pip install --no-cache-dir -r requirements.txt
-
+COPY . .
+ 
+RUN apt-get update && \
+    apt-get install -y default-libmysqlclient-dev gcc && \
+    pip install --no-cache-dir -r requirements.txt
 EXPOSE 5000
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 
@@ -8,11 +9,11 @@ app=Flask(__name__)
 mysql=MySQL()
 
 
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_PORT']=3306
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='dbcrud'
+app.config['MYSQL_HOST']= os.environ.get('MYSQL_HOST','localhost')
+app.config['MYSQL_PORT']=int(os.environ.get('MYSQL_PORT',3306))
+app.config['MYSQL_USER']=os.environ.get('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD']=os.environ.get('MYSQL_PASSWORD', '')
+app.config['MYSQL_DB']=os.environ.get('MYSQL_DB', 'dbcrud')
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 mysql.init_app(app)
 
@@ -86,4 +87,4 @@ def  tarefas_atualizar():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
